3a41f37673f0636bde98fcaa8d514afd183f3d39
Anindita-Panigrahi <aninditapanigrahi04@gmail.com>
2025-04-28 00:53:17 +0530

    You are climbing a staircase. It takes n steps to reach the top.Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?

---
+++ stair.java
@@ -0,0 +1,16 @@
+
+
+public class stair {
+    public static void main(String args[]){
+        int n=3;
+        int prev2=1;
+        int prev=1;
+        for(int i=2;i<=n;i++){
+            int cur_i=prev+prev2;
+            prev2=prev;
+            prev=cur_i;
+        }
+        System.out.println(prev);
+
+    }
+}
