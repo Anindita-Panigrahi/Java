8274916c1e80cc65959a2ef6d2116b75a9ec473a
Anindita-Panigrahi <aninditapanigrahi04@gmail.com>
2025-07-27 23:52:13 +0530

    finding the occurance of sub string with their index

--- string/firstOccurance.java
+++ string/firstOccurance.java
@@ -1,25 +1,25 @@
 public class firstOccurance {
 
-    public static int strStr(String haystack, String needle) {
+    public static void findAllOccurrences(String haystack, String needle) {
         int n = haystack.length();
         int m = needle.length();
 
-        if (m == 0) return 0;
+        if (m == 0) {
+            System.out.println("Empty needle. Every index is a match.");
+            return;
+        }
 
         for (int i = 0; i <= n - m; i++) {
             if (haystack.substring(i, i + m).equals(needle)) {
-                return i;
+                System.out.println("Found at index: " + i);
             }
         }
-
-        return -1;
     }
 
     public static void main(String[] args) {
         String haystack = "sadbutsad";
         String needle = "sad";
 
-        int index = strStr(haystack, needle);
-        System.out.println("First occurrence index: " + index);
+        findAllOccurrences(haystack, needle);
     }
 }
