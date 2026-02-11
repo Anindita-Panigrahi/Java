afde4a62ca666f056ef6a9f2a84e684e917a08ef
Anindita-Panigrahi <aninditapanigrahi04@gmail.com>
2026-02-12 00:23:36 +0530

    Middle of the Linked List

---
+++ linkedList/middleNode.java
@@ -0,0 +1,52 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class middleNode {
+
+    // finding middle node
+    public static ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;        // move 1 step
+            fast = fast.next.next;   // move 2 steps
+        }
+
+        return slow;
+    }
+
+    // Helper to print list
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " -> ");
+            head = head.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+
+        // Create linked list: 1 -> 2 -> 3 -> 4 -> 5 -> 6
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
+
+        System.out.print("Linked List: ");
+        printList(head);
+
+        ListNode mid = middleNode(head);
+
+        System.out.println("Middle Node Value: " + mid.val);
+    }
+}
